3ba2bd15c026cca78c38872c6042b4865902dc87
ysafiedd <yasmina.safi-eddine@inetum.com>
2024-09-03 16:57:29 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, NgbCollapseModule]
 })
 export class AppComponent {
   title = 'portfolio';
+  
 }
